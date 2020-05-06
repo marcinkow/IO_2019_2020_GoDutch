d144fc4a8dab1232e3435fae1a322ffddc8cd4f6
Szymon Jabłoński <szymon.jablonski6@wp.pl>
2020-05-06 17:40:30 +0200

    Dodanie funkcji przeglądania galerii

---
+++ GoDutch/app/src/main/java/com/GoDutch/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.GoDutch.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button_wybierz);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WybierzZGalerii.class));
+            }
+        });
+    }
+}
