ee32523119c8a4142069f7e4b0c2e44a3418bc79
Jędrzej <jedrzejleder@gmail.com>
2020-05-19 14:59:41 +0200

    Dodawanie osób i przekazanie tablicy
    Dodawanie osób do podziały paragonu i przekazanie tablicy z danymi do aktywności  z podziałem rachunku.

--- GoDutch/app/src/main/java/com/GoDutch/myapplication/PodzialParagonu.java
+++ GoDutch/app/src/main/java/com/GoDutch/myapplication/PodzialParagonu.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class PodzialParagonu extends AppCompatActivity {
+
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podzial_paragonu);
+
+        arrayList = getIntent().getStringArrayListExtra("arrayList");
+
     }
 }
