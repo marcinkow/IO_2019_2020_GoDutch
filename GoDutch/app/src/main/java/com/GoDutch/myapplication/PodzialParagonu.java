fb7cb2766562728218a4c70122112be8e557ff5c
Szymon Jabłoński <szymon.jablonski6@wp.pl>
2020-05-29 20:53:09 +0200

    Poprawienie i poprawa funkcjonalności menu podziału paragonu

--- GoDutch/app/src/main/java/com/GoDutch/myapplication/PodzialParagonu.java
+++ GoDutch/app/src/main/java/com/GoDutch/myapplication/PodzialParagonu.java
@@ -1,26 +1,12 @@
 package com.GoDutch.myapplication;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class PodzialParagonu extends AppCompatActivity{
 
@@ -39,8 +25,7 @@ public class PodzialParagonu extends AppCompatActivity{
         setContentView(R.layout.activity_podzial_paragonu);
         lvSpinner = (ListView) findViewById(R.id.listview_spinner);
 
-        BasicSpinnerAdapter adapter = new BasicSpinnerAdapter(napis, liczby_nasze, arrayList, this);
+        SpinnerAdapter adapter = new SpinnerAdapter(napis, liczby_nasze, arrayList, this);
         lvSpinner.setAdapter(adapter);
-
     }
 }
--- GoDutch/app/src/main/java/com/GoDutch/myapplication/SpinnerAdapter.java
+++ GoDutch/app/src/main/java/com/GoDutch/myapplication/SpinnerAdapter.java
@@ -2,8 +2,6 @@ package com.GoDutch.myapplication;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.nfc.Tag;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +11,21 @@ import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
-public class BasicSpinnerAdapter extends BaseAdapter {
+public class SpinnerAdapter extends BaseAdapter {
 
     private ArrayList<String> mSpinnerItems;
     private ArrayList<String> mData;
     private ArrayList<String> mData2;
     private Context mContext;
+    Hashtable<Integer, Integer> spinnerSelected = new Hashtable<Integer, Integer>();
 
-    public BasicSpinnerAdapter(ArrayList<String> data, ArrayList<String> data2, ArrayList<String> spinnerItems, Context context) {
+    public SpinnerAdapter(ArrayList<String> data, ArrayList<String> data2, ArrayList<String> spinnerItems, Context context) {
         mData = data;
         mData2 = data2;
         mContext = context;
@@ -49,22 +48,37 @@ public class BasicSpinnerAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(final int row_position, View convertView, ViewGroup parent) {
 
         View view = convertView;
 
         if (view == null) {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.basic_spinner, null);
+            view = inflater.inflate(R.layout.list_view_spinner, null);
         }
 
         final TextView textView = (TextView) view.findViewById(R.id.row_item_textview);
         final TextView textView1 = (TextView) view.findViewById(R.id.row_item_textview2);
-        Spinner spinner = (Spinner) view.findViewById(R.id.row_item_spinner);
-        textView.setText(mData.get(position));
-        textView1.setText(mData2.get(position));
+        final Spinner spinner = (Spinner) view.findViewById(R.id.row_item_spinner);
+        textView.setText(mData.get(row_position));
+        textView1.setText(mData2.get(row_position));
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item, mSpinnerItems);
         spinner.setAdapter(adapter);
+        if (spinnerSelected.containsKey(row_position)) {
+            spinner.setSelection(spinnerSelected.get(row_position));
+        }
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinnerSelected.put(row_position, position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         view.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -80,12 +94,14 @@ public class BasicSpinnerAdapter extends BaseAdapter {
                 alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "ZAPISZ", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        mData.set(row_position, editText.getText().toString());
                         textView.setText(editText.getText());
                     }
                 });
                 alertDialog1.setButton(DialogInterface.BUTTON_POSITIVE, "ZAPISZ", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        mData2.set(row_position, editText1.getText().toString());
                         textView1.setText(editText1.getText());
                     }
                 });
